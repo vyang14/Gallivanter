127b64a9b6dd4cc2435a48d0ff2ec1af70a76293
Teagrin Forde <teagrinb@gmail.com>
2022-08-02 13:37:19 -0400

    Named Database in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,10 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/TAPiIi
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+DROP DATA IF EXISTS gallivanter_db;
+CREATE DATABASE gallivanter_db;
 
+USE gallivanter_db;
 
 CREATE TABLE `gall_users` (
     `UserID` int  NOT NULL ,
