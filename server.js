77d9fae83b828a68ade79d84c141fcf69f86a72f
Teagrin Forde <teagrinb@gmail.com>
2022-08-02 18:04:53 -0400

    Update controllers index.js and homeRoutes.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(routes); uncomment once routes are complete
+app.use(routes);
 
 sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () =>
