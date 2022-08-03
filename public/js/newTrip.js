fa611250ff6f44d9d38004b3ba6593b6404e4100
vyang14 <vyang1014888@gmail.com>
2022-08-03 17:54:31 -0400

    committing so I can pull from main

--- public/js/newTrip.js
+++ public/js/newTrip.js
@@ -10,115 +10,109 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function initMap() {
     map = new google.maps.Map(document.getElementById("gMap"), {
-        center: { lat: pikaLat, lng: pikaLong },
+        center: { lat: 33.978951, lng: -84.21398 },
         zoom: 13,
     });
 }
 
-const card = document.getElementById("pac-card");
-  const input = document.getElementsByClassName("pac-input");
-  const biasInputElement = document.getElementById(
-    "use-location-bias"
-  );
-  const strictBoundsInputElement = document.getElementById(
-    "use-strict-bounds"
-  );
-  const options = {
-    fields: ["formatted_address", "geometry", "name"],
-    strictBounds: false,
-    types: ["establishment"],
-  };
+// const card = document.getElementById("pac-card");
+//   const input = document.getElementsByClassName("pac-input");
+//   const biasInputElement = document.getElementById(
+//     "use-location-bias"
+//   );
+//   const strictBoundsInputElement = document.getElementById(
+//     "use-strict-bounds"
+//   );
+//   const options = {
+//     fields: ["formatted_address", "geometry", "name"],
+//     strictBounds: false,
+//     types: ["establishment"],
+//   };
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+//   map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
 
-  const autocomplete = new google.maps.places.Autocomplete(input, options);
+//   const autocomplete = new google.maps.places.Autocomplete(input, options);
 
-  // Bind the map's bounds (viewport) property to the autocomplete object,
-  // so that the autocomplete requests use the current map bounds for the
-  // bounds option in the request.
-  autocomplete.bindTo("bounds", map);
+//   autocomplete.bindTo("bounds", map);
 
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById("infowindow-content");
-  infowindow.setContent(infowindowContent);
+//   const infowindow = new google.maps.InfoWindow();
+//   const infowindowContent = document.getElementById("infowindow-content");
+//   infowindow.setContent(infowindowContent);
 
-  const marker = new google.maps.Marker({
-    map,
-    anchorPoint: new google.maps.Point(0, -29),
-  });
+//   const marker = new google.maps.Marker({
+//     map,
+//     anchorPoint: new google.maps.Point(0, -29),
+//   });
 
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
-    marker.setVisible(false);
+//   autocomplete.addListener("place_changed", () => {
+//     infowindow.close();
+//     marker.setVisible(false);
 
-    const place = autocomplete.getPlace();
+//     const place = autocomplete.getPlace();
 
-    if (!place.geometry || !place.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
+//     if (!place.geometry || !place.geometry.location) {
+//       window.alert("No details available for input: '" + place.name + "'");
+//       return;
+//     }
 
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
+//     if (place.geometry.viewport) {
+//       map.fitBounds(place.geometry.viewport);
+//     } else {
+//       map.setCenter(place.geometry.location);
+//       map.setZoom(17);
+//     }
 
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
+//     marker.setPosition(place.geometry.location);
+//     marker.setVisible(true);
 
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent =
-      place.formatted_address;
-    infowindow.open(map, marker);
-  });
+//     infowindowContent.children["place-name"].textContent = place.name;
+//     infowindowContent.children["place-address"].textContent =
+//       place.formatted_address;
+//     infowindow.open(map, marker);
+//   });
 
-  function setupClickListener(id, types) {
-    const radioButton = document.getElementById(id);
+//   function setupClickListener(id, types) {
+//     const radioButton = document.getElementById(id);
 
-    radioButton.addEventListener("click", () => {
-      autocomplete.setTypes(types);
-      input.value = "";
-    });
-  }
+//     radioButton.addEventListener("click", () => {
+//       autocomplete.setTypes(types);
+//       input.value = "";
+//     });
+//   }
 
-  setupClickListener("changetype-all", []);
-  setupClickListener("changetype-address", ["address"]);
-  setupClickListener("changetype-establishment", ["establishment"]);
-  setupClickListener("changetype-geocode", ["geocode"]);
-  setupClickListener("changetype-cities", ["(cities)"]);
-  setupClickListener("changetype-regions", ["(regions)"]);
+//   setupClickListener("changetype-all", []);
+//   setupClickListener("changetype-address", ["address"]);
+//   setupClickListener("changetype-establishment", ["establishment"]);
+//   setupClickListener("changetype-geocode", ["geocode"]);
+//   setupClickListener("changetype-cities", ["(cities)"]);
+//   setupClickListener("changetype-regions", ["(regions)"]);
 
-  biasInputElement.addEventListener("change", () => {
-    if (biasInputElement.checked) {
-      autocomplete.bindTo("bounds", map);
-    } else {
-      autocomplete.unbind("bounds");
-      autocomplete.setBounds({ east: 180, west: -180, north: 90, south: -90 });
-      strictBoundsInputElement.checked = biasInputElement.checked;
-    }
+//   biasInputElement.addEventListener("change", () => {
+//     if (biasInputElement.checked) {
+//       autocomplete.bindTo("bounds", map);
+//     } else {
+//       autocomplete.unbind("bounds");
+//       autocomplete.setBounds({ east: 180, west: -180, north: 90, south: -90 });
+//       strictBoundsInputElement.checked = biasInputElement.checked;
+//     }
 
-    input.value = "";
-  });
+//     input.value = "";
+//   });
 
-  strictBoundsInputElement.addEventListener("change", () => {
-    autocomplete.setOptions({
-      strictBounds: strictBoundsInputElement.checked,
-    });
+//   strictBoundsInputElement.addEventListener("change", () => {
+//     autocomplete.setOptions({
+//       strictBounds: strictBoundsInputElement.checked,
+//     });
 
-    if (strictBoundsInputElement.checked) {
-      biasInputElement.checked = strictBoundsInputElement.checked;
-      autocomplete.bindTo("bounds", map);
-    }
+//     if (strictBoundsInputElement.checked) {
+//       biasInputElement.checked = strictBoundsInputElement.checked;
+//       autocomplete.bindTo("bounds", map);
+//     }
 
-    input.value = "";
-  });
+//     input.value = "";
+//   });
 
-window.initMap = initMap;
-export {};
+// window.initMap = initMap;
+// export {};
             
 window.initMap = initMap;
