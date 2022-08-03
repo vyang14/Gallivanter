c77a6e2075e683be2e8d83e607b232af89d9efad
cwrightj <cwrightj@umich.edu>
2022-08-02 20:20:07 -0400

    e

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -33,7 +33,13 @@ router.get('/:id', async (req, res) => {
 // CREATE a reader
 router.post('/', async (req, res) => {
   try {
-    const userData = await Reader.create(req.body);
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
     res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
