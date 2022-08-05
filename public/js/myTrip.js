d1a8957d71f2af503420d31aafdece2c9f19a309
vyang14 <vyang1014888@gmail.com>
2022-08-05 03:15:17 -0400

    fixed post to trip table

--- public/js/myTrip.js
+++ public/js/myTrip.js
@@ -2,3 +2,8 @@ import { yelpFetch } from "./yelp";
 import *  as Utils from "./utils";
 
 window.init = Utils.initMap();
+
+var userData = {
+    method: 'GET',
+    url: 'localhost:3001/api/trips'
+}
--- public/js/newTrip.js
+++ public/js/newTrip.js
@@ -8,7 +8,7 @@ var userInput = {};
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems, {
-        format: 'mm-dd-yyyy',
+        format: 'yyyy-dd-mm',
         minDate: new Date(),
         firstDay: 0,
 });
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Dropdown.init(elems, {});
 });
 
-for(var i = 0; i < 3; i++){
+for(var i = 0; i < 2; i++){
     let iteration = 'a'+i;
     var listEl = document.getElementById(iteration);
 
@@ -33,11 +33,9 @@ for(var i = 0; i < 3; i++){
             prompt('Please do not leave any forms blank!');
             return;
         }
-
-        const travelType = dropButton.textContent
         dropButton.textContent = this.lastChild.lastChild.textContent;
         allBtns.forEach(allBtns => allBtns.disabled=true);
-        return {location, destination, departDate, arriveDate, travelType};
+        newTripFormhandler(event);
         })
 }
 
@@ -46,6 +44,7 @@ swapButton.addEventListener('click',function(){
     var destination = document.getElementById('goingTo');
     var input1 = document.getElementById('departForm').value;
     var input2 = document.getElementById('goingTo').value;
+    
     depart.value = input2;
     destination.value = input1;
 })
@@ -55,15 +54,11 @@ const newTripFormhandler = async (event) => {
   
     var depart = document.getElementById('departForm');
     var destination = document.getElementById('goingTo');
-    var input1 = document.getElementById('departForm').value;
-    var input2 = document.getElementById('goingTo').value;
-    depart.value = input2;
-    destination.value = input1;
     var departDate = document.getElementById('dateLeave').value;
     var arriveDate = document.getElementById('dateReturn').value;
-    method= "AIRPORT"
-    if (depart,destination,departDate,arriveDate) {
-      const response = await fetch('/api/users', {
+    var method = dropButton.textContent;
+    if (depart&&destination&&departDate&&arriveDate) {
+      const response = await fetch('/api/trip', {
         method: 'POST',
         body: JSON.stringify({ depart,destination,departDate,arriveDate,method}),
         headers: { 'Content-Type': 'application/json' },
