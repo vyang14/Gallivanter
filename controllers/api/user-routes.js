e4a1dd38582a44364ba79953de7e6948101b1a95
Teagrin Forde <teagrinb@gmail.com>
2022-08-08 09:59:53 -0400

    Fixed login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -50,7 +50,6 @@ router.post("/login", async (req, res) => {
       res.status(200).json({ user: userData, message: "You are logged in!" });
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
@@ -66,24 +65,24 @@ router.post("/logout", (req, res) => {
   }
 });
 
-// DELETE a User
-router.delete('/:id', async (req, res) => {
-  try {
-    const userData = await User.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+// DELETE a User can be added later
+// router.delete('/:id', async (req, res) => {
+//   try {
+//     const userData = await User.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-    if (!userData) {
-      res.status(404).json({ message: 'No user found with that id!' });
-      return;
-    }
+//     if (!userData) {
+//       res.status(404).json({ message: 'No user found with that id!' });
+//       return;
+//     }
 
-    res.status(200).json(userData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.status(200).json(userData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,12 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require('bcrypt');
+const sequelize = require("../config/connection");
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+ }
 
 User.init(
   {
@@ -32,11 +37,21 @@ User.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updateUserData) => {
+        updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+        return updateUserData;
+      },
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
